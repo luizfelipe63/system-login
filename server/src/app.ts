fc233def39633c5cc6dad7872c156352bdb1d441
Felipe <75274860+luizfelipe63@users.noreply.github.com>
2024-02-19 19:00:48 -0300

    Feat:JWT implementation

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,8 +2,23 @@ import fastify from 'fastify'
 import { ZodError } from 'zod'
 import { env } from './env'
 import { routes } from './http/controllers/users/routes'
+import fastifyJwt from '@fastify/jwt'
+import fastifyCookie from '@fastify/cookie'
 
 export const app = fastify()
+
+app.register(fastifyJwt, {
+  secret: env.JWT_ENV,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false,
+  },
+  sign: {
+    expiresIn: '10m',
+  },
+})
+
+app.register(fastifyCookie)
 
 app.register(routes)
 
---
+++ server/src/http/controllers/users/authenticate.ts
@@ -0,0 +1,65 @@
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-user-repository'
+import { AuthenticateUseCase } from '@/use-cases/authenticate'
+import { InvalidCredentialsError } from '@/use-cases/errors/invalid-credentials-error'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+export async function authenticate(
+  request: FastifyRequest,
+  reply: FastifyReply,
+) {
+  const authenticateBodySchema = z.object({
+    email: z.string(),
+    password: z.string().min(6),
+  })
+
+  const { email, password } = authenticateBodySchema.parse(request.body)
+
+  try {
+    const prismaUsersRepository = new PrismaUsersRepository()
+    const authenticateUserCase = new AuthenticateUseCase(prismaUsersRepository)
+
+    const { user } = await authenticateUserCase.execute({
+      email,
+      password,
+    })
+
+    const token = await reply.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+        },
+      },
+    )
+
+    const refreshToken = await reply.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+          expiresIn: '7d',
+        },
+      },
+    )
+
+    return reply
+      .setCookie('refreshToken', refreshToken, {
+        path: '/',
+        secure: true,
+        sameSite: true,
+        httpOnly: true,
+      })
+      .status(200)
+      .send({
+        token,
+      })
+  } catch (err) {
+    if (err instanceof InvalidCredentialsError) {
+      return reply.status(409).send({
+        message: err.message,
+      })
+    }
+    return err
+  }
+}

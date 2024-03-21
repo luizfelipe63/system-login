5fbcb4f95039e068434fe357a933cc8db822cc24
Felipe <75274860+luizfelipe63@users.noreply.github.com>
2024-03-21 17:19:00 -0300

    Feat: Create router generate OTP

--- server/src/http/controllers/users/routes.ts
+++ server/src/http/controllers/users/routes.ts
@@ -1,8 +1,10 @@
 import { FastifyInstance } from 'fastify'
 import { register } from './register'
 import { authenticate } from './authenticate'
+import { twoFAGenerate } from './2fa-generate'
 
 export async function routes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+  app.post('/auth/generate', twoFAGenerate)
 }

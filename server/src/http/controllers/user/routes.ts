5c7364df8611c81506a2820b27a29d7e6c434663
Luiz Felipe <75274860+luizfelipe63@users.noreply.github.com>
2025-02-19 11:35:44 -0300

    Rename file

--- server/src/http/controllers/user/routes.ts
+++ server/src/http/controllers/user/routes.ts
@@ -4,7 +4,7 @@ import { authenticate } from './authenticate'
 import { profile } from './profile'
 import { verifyJwt } from '@/http/middlewares/verify-jwt'
 
-export async function routes(app: FastifyInstance) {
+export async function userRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
   app.get('/me', {onRequest: [verifyJwt]}, profile)

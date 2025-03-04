66035685cddbeaa4882f4e86b6ed1ecfda5de787
Luiz Felipe <75274860+luizfelipe63@users.noreply.github.com>
2025-03-04 11:08:08 -0300

    Create ui input

---
+++ web/src/components/ui/input.tsx
@@ -0,0 +1,14 @@
+import { ComponentProps, forwardRef } from "react";
+import { twMerge } from "tailwind-merge";
+
+type InputProps = ComponentProps<'input'>
+
+export const Input = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
+    return(
+        <input 
+        {...props}
+        ref={ref}
+        className={twMerge('px-4 h-12 bg-white border border-zinc-200 rounded-lg placeholder-zinc-500 outline-none text-sm', props.className)}
+        />
+    )
+})

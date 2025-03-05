3b127a3781a1dc54ae8e75f8398ec46a38fbf531
Luiz Felipe <75274860+luizfelipe63@users.noreply.github.com>
2025-03-05 11:48:05 -0300

    style: config lint biome

--- web/src/App.tsx
+++
@@ -1,7 +0,0 @@
-function App() {
-  return (
-    <h1 className="text-amber-300">Hello wolrd</h1>
-  )
-}
-
-export default App
--- web/src/components/ui/input.tsx
+++ web/src/components/ui/input.tsx
@@ -1,14 +1,17 @@
-import { ComponentProps, forwardRef } from "react";
-import { twMerge } from "tailwind-merge";
+import { type ComponentProps, forwardRef } from 'react'
+import { twMerge } from 'tailwind-merge'
 
 type InputProps = ComponentProps<'input'>
 
 export const Input = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
-    return(
-        <input 
-        {...props}
-        ref={ref}
-        className={twMerge('px-4 h-12 bg-white border border-zinc-200 rounded-lg placeholder-zinc-500 outline-none text-sm', props.className)}
-        />
-    )
+  return (
+    <input
+      {...props}
+      ref={ref}
+      className={twMerge(
+        'px-4 h-12 bg-white border border-zinc-200 rounded-lg placeholder-zinc-500 outline-none text-sm',
+        props.className
+      )}
+    />
+  )
 })

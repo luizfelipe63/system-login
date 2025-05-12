8fa3c07ec6830e4f5b8e8b33fd792017f26f8003
Luiz Felipe <75274860+luizfelipe63@users.noreply.github.com>
2025-05-12 11:56:04 -0300

    Feat: create page profile

---
+++ web/src/pages/profile.tsx
@@ -0,0 +1,36 @@
+import ilustacao from '../assets/illustration.png'
+
+export function Profile() {
+  return (
+    <div className="mx-16 py-6">
+      <div className="flex flex-col gap-2 pb-7">
+        <h1 className="text-3xl  text-zinc-900">Profile</h1>
+        <p className=" text-zinc-700">View all your profile details here.</p>
+      </div>
+      <div className="border border-blue-300 flex justify-around py-6">
+        <div className="flex flex-col items-center">
+          <h1 className="text-2xl  text-zinc-900">
+            Seja bem vindo <span className="text-blue-600">Luiz Felipe</span>
+          </h1>
+          <img src={ilustacao} width={250} alt="" />
+        </div>
+        <div className="flex flex-col gap-3">
+          <h1 className="text-2xl text-zinc-900">Two-factor authentication</h1>
+          <p className="w-xs text-justify text-zinc-700">
+            Two-factor authentication enhances security by requiring both a
+            password and a second verification method to access an account.
+          </p>
+          <div className="flex flex-col gap-4">
+            <h1 className=" text-zinc-900">Two-factor methods</h1>
+            <div className="px-4 py-3 flex justify-between rounded items-center border border-blue-300 bg-blue-50">
+              <h2 className=" text-zinc-700">Authenticator app</h2>
+              <button className="bg-blue-600 text-xs p-2 rounded-lg text-zinc-100 cursor-pointer">
+                ADD
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
--- web/src/routes/routes.tsx
+++ web/src/routes/routes.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router'
 import { Register } from '../pages/register'
 import { Authenticate } from '../pages/authenticate'
+import { Profile } from '../pages/profile'
 
 export function Router() {
   return (
     <Routes>
       <Route path="/signup" element={<Register />} />
       <Route path="/login" element={<Authenticate />} />
+      <Route path="/me" element={<Profile />} />
     </Routes>
   )
 }

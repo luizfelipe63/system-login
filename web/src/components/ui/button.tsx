6314548cfa560b8921919f02ab7ed8c56cbc53fb
Luiz Felipe <75274860+luizfelipe63@users.noreply.github.com>
2025-05-14 14:07:12 -0300

    Feat: create variants button

--- web/src/components/ui/button.tsx
+++ web/src/components/ui/button.tsx
@@ -1,17 +1,41 @@
 import { type ComponentProps, forwardRef } from 'react'
-import { twMerge } from 'tailwind-merge'
+import { tv, type VariantProps } from 'tailwind-variants';
 
-type ButtonProps = ComponentProps<'button'>
+const button = tv({
+  base: 'bg-blue-600 p-4 rounded-lg text-zinc-100 cursor-pointer',
+
+  variants: {
+    color: {
+      primary: 'bg-blue-600 text-zinc-100',
+      secondary: 'bg-blue-100 text-zinc-900'
+    },
+    size: {
+      sm: 'text-sm',
+      md: 'text-base',
+      lg: 'px-4 py-3 text-lg'
+    },
+    compoundVariants: [
+      {
+        size: ['sm', 'md'],
+        class: 'px-3 py-1'
+      }
+    ],
+    defaultVariants: {
+      size: 'md',
+      color: 'primary'
+    }
+  }
+})
+
+type ButtonProps = ComponentProps<'button'> & VariantProps<typeof button>
 
 export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-  (props, ref) => {
+  ({ className, color, size, ...props }, ref) => {
     return (
       <button
         {...props}
         ref={ref}
-        className={twMerge(
-          'bg-blue-600 p-4 rounded-lg text-zinc-100 cursor-pointer',
-        )}
+        className={button({ color, size, className })}
       />
     )
   },
